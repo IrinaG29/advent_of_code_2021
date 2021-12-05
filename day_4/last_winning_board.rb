c87428fc820bcfea1e993f902c18882c7239b1d8
IrinaG29 <36776772+IrinaG29@users.noreply.github.com>
2021-12-05 18:43:33 +0000

    part 2 solution

---
+++ day_4/last_winning_board.rb
@@ -0,0 +1,123 @@
+class Board
+  attr_reader :matrix, :table
+
+  def initialize(matrix:)
+    @matrix = matrix
+    @table = convert_matrix_to_table
+  end
+
+  def convert_matrix_to_table
+    matrix.map do |row|
+      row.map do |cell|
+        Cell.new(number: cell)
+      end
+    end
+  end
+
+  def play_number(number:)
+    found_cell = cells.find do |cell|
+      cell.number == number
+    end
+
+    if found_cell
+      found_cell.mark!
+    end
+  end
+
+  def winner?
+    found_winning_row = table.find do |row|
+      row.all? { |cell| cell.marked? }
+    end
+
+    return found_winning_row if found_winning_row
+
+    found_winning_column = table.transpose.find do |col|
+      col.all? { |cell| cell.marked? }
+    end
+
+    return found_winning_column if found_winning_column
+
+    nil
+  end
+
+  def unmarked_numbers
+    cells.select do |cell|
+      cell.unmarked?
+    end.map(&:number)
+  end
+
+  private
+
+  def cells
+    table.flatten
+  end
+end
+
+class Cell
+  attr_reader :number
+
+  def initialize(number:)
+    @number = number
+    @marked = false
+  end
+
+  def marked?
+    @marked
+  end
+
+  def unmarked?
+    !marked?
+  end
+
+  def mark!
+    @marked = true
+  end
+end
+
+def board_inputs
+  File.read("bingo.txt").split("\n\n")[1..-1]
+end
+
+# input
+# => " 5 43 13 47 93\n25 78 64 56 10\n75 90 50 72 14\n 9 29 58 79 62\n69 66 88 35 16"
+# output
+# array of arrays?
+def convert_board_input_to_matrix(board_input)
+  board_input.split("\n").map do |row|
+    row.strip.split(/\s+/).map(&:to_i)
+  end
+end
+
+def boards
+  @boards ||= board_inputs.map do |board_input|
+    Board.new(matrix: convert_board_input_to_matrix(board_input))
+  end
+end
+
+def announced_numbers
+  File.read("bingo.txt").split("\n\n")[0].split(",").map { |s| s.to_i }
+end
+
+def play_number(number:)
+  boards.each do |board|
+    if !board.winner?
+      board.play_number(number: number)
+    end
+  end
+end
+
+all_winning_boards = []
+last_winning_number = 0
+
+announced_numbers.each do |number|
+  play_number(number: number)
+
+  boards.each do |board|
+    if board.winner? && !all_winning_boards.include?(board)
+      all_winning_boards << board
+      last_winning_number = number
+    end
+  end
+end
+
+puts all_winning_boards[-1].unmarked_numbers.sum * last_winning_number
