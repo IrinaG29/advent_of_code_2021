3b135b4fca1e8c5b74e36e199e6515af5e29ec80
IrinaG29 <36776772+IrinaG29@users.noreply.github.com>
2021-12-14 23:27:16 +0000

    found horizontal and vertical lines

--- day_5/hydrothermal_vents.rb
+++ day_5/hydrothermal_vents.rb
@@ -9,4 +9,12 @@ end
 to_integers = nested_arrays.map { |a_1| a_1.map { |a_2| a_2.map { |s| s.to_i }}}
 to_hash = to_integers.map { |a_1| a_1.map { |a_2| { x: a_2[0], y: a_2[1] }}}
 
+def horizontal_vertical_lines(to_hash)
+  to_hash.select { |a| a[0][:x] == a[1][:x] || a[0][:y] == a[1][:y] }
+end
+
+def overlapping_lines
+  grid = []
+  
+end
 binding.pry
